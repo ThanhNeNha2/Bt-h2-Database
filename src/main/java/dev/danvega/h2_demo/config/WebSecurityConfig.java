a151235d56182da95430a1dc312030361f907e9f
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-11-22 22:52:12 +0700

    hihi

--- src/main/java/dev/danvega/h2_demo/config/WebSecurityConfig.java
+++ src/main/java/dev/danvega/h2_demo/config/WebSecurityConfig.java
@@ -3,64 +3,77 @@ package dev.danvega.h2_demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import dev.danvega.h2_demo.service.CustomUserDetailsService;
+import dev.danvega.h2_demo.service.LoginService;
+import dev.danvega.h2_demo.service.UserAuthService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-        private final CustomUserDetailsService userDetailsService;
+    // private final CustomUserDetailsService userDetailsService;
 
-        public WebSecurityConfig(CustomUserDetailsService userDetailsService) {
-                this.userDetailsService = userDetailsService;
-        }
+    // public WebSecurityConfig(CustomUserDetailsService userDetailsService) {
+    // this.userDetailsService = userDetailsService;
+    // }
 
-        @Bean
-        public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-                AuthenticationManagerBuilder authenticationManagerBuilder = http
-                                .getSharedObject(AuthenticationManagerBuilder.class);
-                authenticationManagerBuilder.userDetailsService(userDetailsService);
-                return authenticationManagerBuilder.build();
-        }
+    // @Bean
+    // public AuthenticationManager authManager(HttpSecurity http) throws Exception
+    // {
+    // AuthenticationManagerBuilder authenticationManagerBuilder = http
+    // .getSharedObject(AuthenticationManagerBuilder.class);
+    // authenticationManagerBuilder.userDetailsService(userDetailsService);
+    // return authenticationManagerBuilder.build();
+    // }
 
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                http
-                                .csrf().disable() // Tắt CSRF cho API
-                                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                                                .requestMatchers("/api/login", "/api/register", "/api/user",
-                                                                "/api/addUser", "/api/delete/{id}", "/api/update/{id}",
-                                                                "/register", "/api/userAuth")
-                                                .permitAll() // Cho phép tất cả truy cập vào API login và register
-                                                .requestMatchers("/companies", "/nV_CP").hasRole("USER") // Cấu hình cho
-                                                                                                         // người dùng
-                                                                                                         // có vai trò
-                                                                                                         // "USER"
-                                                                                                         // Admin có
-                                                                                                         // quyền truy
-                                                                                                         // cập tất cả
+    @Bean
+    public UserDetailsService userDetailsService(LoginService loginService) {
+        return new CustomUserDetailsService(loginService);
+    }
 
-                                                .requestMatchers("/userAuthTable").hasRole("ADMIN") // Chỉ cho phép
-                                                                                                    // ADMIN truy cập
-                                                                                                    // vào
-                                                                                                    // /userAuthTable
-                                                .anyRequest().authenticated() // Các yêu cầu khác yêu cầu đăng nhập
-                                )
-                                .httpBasic() // Kích hoạt HTTP Basic Authentication cho API
-                                .and()
-                                .formLogin(form -> form
-                                                // .requestMatchers("/**").hasRole("ADMIN")
-                                                .loginPage("/login") // Đường dẫn trang đăng nhập của bạn
-                                                .defaultSuccessUrl("/", true) // Trang chuyển hướng sau khi đăng nhập
-                                                                              // thành công
-                                                .permitAll() // Cho phép tất cả truy cập vào trang đăng nhập và đăng ký
-                                );
+    @Bean
+    public DaoAuthenticationProvider authProvider(
+            PasswordEncoder passwordEncoder,
+            UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        ;
+        // authProvider.setHideUserNotFoundExceptions(false);
+        return authProvider;
+    }
 
-                return http.build(); // Dùng build() để tạo SecurityFilterChain
-        }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+
+                        .requestMatchers("/api/register", "/api/userAuth", "/api/generateToken", "/h2-console/*")
+                        .permitAll()
+
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/companies").hasRole("ADMIN")
+                        .requestMatchers("/register").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic() // Kích hoạt HTTP Basic Authentication cho API
+                .and()
+                .formLogin(form -> form
+
+                        .loginPage("/login") // Đường dẫn trang đăng nhập của bạn
+                        .defaultSuccessUrl("/userAuthTable", true) // Trang chuyển hướng sau khi đăng nhập
+                        // thành công
+                        .permitAll() // Cho phép tất cả truy cập vào trang đăng nhập và đăng ký
+                );
+
+        return http.build(); // Dùng build() để tạo SecurityFilterChain
+    }
 }
